69cb02127a45ebce3440f58ed0cb5675465601ce
JennyFranz99 <jfranz@captechu.edu>
2025-07-30 19:44:34 -0400

    Update components and layout

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn page heading', () => {
-  render(<App />);
+render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const headingElement = screen.getByText(/welcome to the plastic pollution awareness platform/i);
   expect(headingElement).toBeInTheDocument();
 });
