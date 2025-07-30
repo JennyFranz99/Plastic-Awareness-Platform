3c509ea9bc8653f091363c52f3f25766c63e9c48
JennyFranz99 <149439432+JennyFranz99@users.noreply.github.com>
2025-07-30 18:32:48 -0400

    Delete App.test.js

--- App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn page heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/welcome to the plastic pollution awareness platform/i);
-  expect(headingElement).toBeInTheDocument();
-});
