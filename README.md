d9b5458df692e335daa61e74e21b75be0d31659b
JennyFranz99 <jfranz@captechu.edu>
2025-08-06 20:15:31 -0400

    Update app.test

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Setup
+
+Run `npm install` to install project dependencies, including Recharts for the dashboard.
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.test.js
+++ src/App.test.js
@@ -2,23 +2,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-
-
-// Recharts is mocked to avoid requiring the actual library during tests.
-jest.mock(
-  'recharts',
-  () => ({
-    ResponsiveContainer: ({ children }) => <div>{children}</div>,
-    LineChart: ({ children }) => <div>{children}</div>,
-    Line: () => null,
-    XAxis: () => null,
-    YAxis: () => null,
-    CartesianGrid: () => null,
-    Tooltip: () => null,
-  }),
-  { virtual: true }
-);
-
 import App from './App';
 
 test('renders home page hero', () => {
