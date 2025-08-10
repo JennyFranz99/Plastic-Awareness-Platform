62331876ca52eb443b7845382c2708bff6893074
JennyFranz99 <jfranz@captechu.edu>
2025-08-10 18:03:57 -0400

    Update Feedback

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDHolNcJ52tQEh6mK9h-9a_pjKO-tnrGdQ",
+  authDomain: "plastic-awareness-platform.firebaseapp.com",
+  projectId: "plastic-awareness-platform",
+  storageBucket: "plastic-awareness-platform.firebasestorage.app",
+  messagingSenderId: "927568247152",
+  appId: "1:927568247152:web:353494942c7688688177b5",
+  measurementId: "G-8Z56EQJX9H"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
